1367c9f334d296257fd7a0865fb6bd04a5e015f3
u6tory <30873030+u6tory@users.noreply.github.com>
2020-06-15 00:17:24 +0900

    Basement of Exress

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hi!')
+const express = require("express");
+const app = express();
+
+const handleListening = () => {
+    console.log("listening on port 4000");
+}
+
+app.listen(4000, handleListening);
