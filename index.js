6fc307146ef598e12d98d6d311b988bd8e513bef
u6tory <30873030+u6tory@users.noreply.github.com>
2020-06-15 01:04:07 +0900

    with nodemon

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const handleListening = () => {
 
 const handleHome = (req, res) => { 
     console.log(req);            // 유저가 보낸 정보는 req Obj.에 담기고
-    res.send("hello from home"); // 우리는 res. 객체의 send로 보낸다.
+    res.send("hello from my ass"); // 우리는 res. 객체의 send로 보낸다.
 }
 
 app.get('/', handleHome); // get으로 '/' 경로로 요청이 들어오면 handleHome 함수를 실행한다.
