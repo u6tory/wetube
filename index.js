fff4b8f04f9f115415d7fb8fac3c819a3fb67831
u6tory <30873030+u6tory@users.noreply.github.com>
2020-06-15 00:33:14 +0900

    Handling routes with Express

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ const handleListening = () => {
     console.log("listening on port 4000");
 }
 
+const handleHome = (req, res) => { 
+    console.log(req);            // 유저가 보낸 정보는 req Obj.에 담기고
+    res.send("hello from home"); // 우리는 res. 객체의 send로 보낸다.
+}
+
+app.get('/', handleHome);
+
 app.listen(4000, handleListening);
