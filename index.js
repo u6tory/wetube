e069591344de76a24db41fc0263435ceecb140b8
u6tory <30873030+u6tory@users.noreply.github.com>
2020-06-16 10:33:47 +0900

    First time with  middleware concept

--- index.js
+++ index.js
@@ -11,10 +11,15 @@ const handleHome = (req, res) => {
     res.send("hello from my ass"); // 우리는 res. 객체의 send로 보낸다.
 }
 
+const betweenHome = (req, res, next) =>{  // middleware 뿐만 아니라 모두 다음으로 진행시키는 next라는 arg를 가진다. 
+    console.log("I'm middleware");
+    next();
+}
+
 const handleProfile = (req, res) => res.send(`You're on my profile`);
 
-app.get('/', handleHome); // get으로 '/' 경로로 요청이 들어오면 handleHome 함수를 실행한다.
-app.get('/profile', handleProfile);
+app.get('/', handleHome); 
+app.get('/profile', betweenHome, handleProfile); // betweenHome 이라는 middleware 함수를 거쳤다가 handleProfile로 이동된다.
 
 app.listen(PORT, handleListening);
 
