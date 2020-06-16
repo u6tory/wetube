241ab74a614f958151f24ec327e16ab979f074da
u6tory <30873030+u6tory@users.noreply.github.com>
2020-06-16 18:23:34 +0900

    MVC:Controller - Video detail and upload setup

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import {videos} from "../db"
+import routes from "../routes";
 
 export const home = (req, res) => res.render("home", {pageTitle: "Home", videos});
 export const search = (req, res) => {
@@ -6,4 +7,19 @@ export const search = (req, res) => {
     const {query: {search: searchingBy}} = req;
 //  const {search: searchingBy} = req.query; 로도 가능하다.
     res.render("search", {pageTitle:"Search", searchingBy, videos}); // searchingBy: searchingBy 로도 가능
+}
+export const videoDetail = (req, res) => {
+    res.render("videoDetail", {pageTitle: "videoDetail", videos})
+};
+
+export const getUpload = (req, res) =>{
+    res.render("upload", {pageTitle: "upload"})
+}
+
+export const postUpload = (req, res) =>{
+    const {
+        body:{file, title, description}
+    }=req;
+    // To-Do: upload and save video
+    res.redirect(routes.videoDetail(324393))
 }
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import routes from "../routes";
+import { videoDetail, getUpload, postUpload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, (req, res) => res.send("Videos"));
-videoRouter.get(routes.upload, (req, res) => res.send("Upload"));
-videoRouter.get(routes.videoDetail, (req, res) => res.send("Video Detail"));
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
+videoRouter.get(routes.videoDetail(), videoDetail); // videoDetail이 routes에서 설정된 함수이기 때문에 ()를 붙여야 함.
 videoRouter.get(routes.editVideo, (req, res) => res.send("Edit Video"));
 videoRouter.get(routes.deleteVideo, (req, res) => res.send("Delete Video"));
 
