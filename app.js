9d8ba051cbfc59b03cddf69bcbdfc3b2710cb65e
u6tory <30873030+u6tory@users.noreply.github.com>
2020-06-16 11:47:51 +0900

    all router setup with routes.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import bodyParser from "body-parser"; // body data 분석 like json, urlencoded
 import userRouter from "./routers/userRouter.js"
 import globalRouter from "./routers/globalRouter.js"
 import videoRouter from "./routers/videoRouter.js"
+import routes from "./routes"
 
 const app = express();
 
@@ -15,9 +16,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app; // 이 파일을 import 하면 app을 기본적으로 내보낸다.
 
