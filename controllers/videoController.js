37d29d6c9bfdba4405314809df9700acdf3a2875
u6tory <30873030+u6tory@users.noreply.github.com>
2020-06-16 16:52:26 +0900

    MVC: Controller - POST data & bug fixes

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -5,5 +5,5 @@ export const search = (req, res) => {
     console.log(req.query); // = {search:"검색어"} 로 출력됨.
     const {query: {search: searchingBy}} = req;
 //  const {search: searchingBy} = req.query; 로도 가능하다.
-    res.render("search", {pageTitle:"Search", searchingBy}); // searchingBy: searchingBy 로도 가능
+    res.render("search", {pageTitle:"Search", searchingBy, videos}); // searchingBy: searchingBy 로도 가능
 }
