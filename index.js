57e9eb364d003c872dc6444ed7b6206345b5f3bb
u6tory <30873030+u6tory@users.noreply.github.com>
2020-06-16 10:45:54 +0900

    set basic middleWares like morgan,helmet,godyparser,cookieparser

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express"; // 좀 더 현대적인 형태
+import helmet from "helmet"; // Security
+import morgan from "morgan"; // Logging
+import cookieParser from "cookie-parser"; // 쿠키 분석 툴
+import bodyParser from "body-parser"; // body data 분석 like json, urlencoded
+
 const app = express();
 const PORT = 4000;
 
@@ -10,16 +15,16 @@ const handleHome = (req, res) => {
     console.log(req);            // 유저가 보낸 정보는 req Obj.에 담기고
     res.send("hello from my ass"); // 우리는 res. 객체의 send로 보낸다.
 }
-
-const betweenHome = (req, res, next) =>{  // middleware 뿐만 아니라 모두 다음으로 진행시키는 next라는 arg를 가진다. 
-    console.log("I'm middleware");
-    next();
-}
-
 const handleProfile = (req, res) => res.send(`You're on my profile`);
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));
+
 app.get('/', handleHome); 
-app.get('/profile', betweenHome, handleProfile); // betweenHome 이라는 middleware 함수를 거쳤다가 handleProfile로 이동된다.
+app.get('/profile', handleProfile); // betweenHome 이라는 middleware 함수를 거쳤다가 handleProfile로 이동된다.
 
 app.listen(PORT, handleListening);
 
