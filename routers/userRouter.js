d3b296b02131bac913b8f3fc036588ad4ebc11a2
u6tory <30873030+u6tory@users.noreply.github.com>
2020-06-16 15:37:34 +0900

    MVC: Controller - HTML forms setup

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
 import routes from "../routes";
+import { editProfile, userDetail, changePassword } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, (req, res) => res.send("Users"));
-userRouter.get(routes.userDetail, (req, res) => res.send("User Detail"));
-userRouter.get(routes.editProfile, (req, res) => res.send("Edit Profile"));
-userRouter.get(routes.changePassword, (req, res) => res.send("Change Password"));
+userRouter.get(routes.editProfile, editProfile); // userDetail 보다 아래에 있으면 :id 로 인식하니 주의
+userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
