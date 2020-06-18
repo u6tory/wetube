ad8592b5202275868c9dd32837e0a750b27e86ad
u6tory <30873030+u6tory@users.noreply.github.com>
2020-06-18 13:03:44 +0900

    mongoDB: Schema Setup

--- init.js
+++ init.js
@@ -1,11 +1,14 @@
 import "./db";
 import app from "./app"; // npm모듈이 아니므로 ./으로 경로명을 써주어야 함
 import dotenv from "dotenv";
+import "./models/Video"; // export default 해주었기 때문에 바로 파일을 지정해준다.
+import "./models/Comments";
 
-dotenv.config()
+dotenv.config();
 
 const PORT = process.env.PORT;
 
-const handleListening = () => console.log(`listening on http://localhost:${PORT}`);
+const handleListening = () =>
+  console.log(`listening on http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
