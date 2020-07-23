58c2844d14fad363226b051509e31bdd0247a200
u6tory <30873030+u6tory@users.noreply.github.com>
2020-07-23 19:29:12 +0900

    url changed

--- src/passport.js
+++ src/passport.js
@@ -15,7 +15,7 @@ passport.use(
     {
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
-      callbackURL: `http://localhost:4000${routes.githubCallback}`,
+      callbackURL: `https://shielded-harbor-86945.herokuapp.com${routes.githubCallback}`,
     },
     githubLoginCallback
   )
